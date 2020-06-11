ea8eab484d43dc420e280e6803b9742241024d20
Kafka <aiya0422@gmail.com>
2020-06-11 14:25:09 +0900

    Scene name change to Move_Scene

--- Assets/Script/MainSystem.cs
+++ Assets/Script/MainSystem.cs
@@ -21,10 +21,14 @@ public class MainSystem : MonoBehaviour
         Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //get mousepointer's position
         RaycastHit2D hit = Physics2D.Raycast(mouse_pos, Vector2.zero, 0f); //shooting ray for find object with collide
 
-        if (Input.GetMouseButtonDown(0)) //check left click
+        if (Input.GetMouseButtonDown(0)) //left click event
         {
             if (hit.collider != null) //do when ray hit object with collide
             {
+                if(hit.collider.gameObject.tag == "Unit") //check select object's tag
+                {
+                    Debug.Log("Unit select");
+                }
                 // hit.transform.GetComponent<SpriteRenderer>().color = Color.red; // turn object's collor red for checking
                 return true;
             }
